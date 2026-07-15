8b616c3411ce480f21132401d6f4ab9b249a6953
Sharad <rumanesharad@gmail.com>
2026-07-15 11:01:17 +0530

    Fix AccountRepository: Replace SQL Server stored procedures with EF Core for PostgreSQL compatibility

--- TheBeautyHubData/Repositories/AccountRepository.cs
+++ TheBeautyHubData/Repositories/AccountRepository.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using TheBeautyHubData.Context;
 using TheBeautyHubData.Entities;
@@ -13,7 +11,7 @@ namespace TheBeautyHubData.Repositories
 {
     /// <summary>
     /// Repository implementation for Account entity.
-    /// Uses stored procedures for all CRUD operations via EF Core.
+    /// Uses EF Core for all CRUD operations.
     /// </summary>
     public class AccountRepository : IAccountRepository
     {
@@ -25,104 +23,69 @@ namespace TheBeautyHubData.Repositories
         }
 
         /// <summary>
-        /// Inserts a new account using the usp_Insert_Account stored procedure
+        /// Inserts a new account
         /// </summary>
         public async Task<Account> InsertAccountAsync(Account account)
         {
-            var parameters = new[]
-            {
-                new SqlParameter("@AccountCode", account.AccountCode),
-                new SqlParameter("@AccountName", account.AccountName),
-                new SqlParameter("@AccountType", account.AccountType),
-                new SqlParameter("@Mode", account.Mode),
-                new SqlParameter("@IsUnderTrial", account.IsUnderTrial),
-                new SqlParameter("@TrialStartedOn", (object?)account.TrialStartedOn ?? DBNull.Value),
-                new SqlParameter("@TrialDuration", (object?)account.TrialDuration ?? DBNull.Value),
-                new SqlParameter("@TrialExpiredOn", (object?)account.TrialExpiredOn ?? DBNull.Value),
-                new SqlParameter("@CreatedBy", (object?)account.CreatedBy ?? DBNull.Value)
-            };
-
-            var result = await _context.Accounts
-                .FromSqlRaw("EXEC usp_Insert_Account @AccountCode, @AccountName, @AccountType, @Mode, @IsUnderTrial, @TrialStartedOn, @TrialDuration, @TrialExpiredOn, @CreatedBy", 
-                    parameters)
-                .ToListAsync();
-
-            return result.FirstOrDefault() ?? account;
+            _context.Accounts.Add(account);
+            await _context.SaveChangesAsync();
+            return account;
         }
 
         /// <summary>
-        /// Updates an existing account using the usp_Update_Account stored procedure
+        /// Updates an existing account
         /// </summary>
         public async Task<Account> UpdateAccountAsync(Account account)
         {
-            var parameters = new[]
-            {
-                new SqlParameter("@AccountId", account.AccountId),
-                new SqlParameter("@AccountCode", account.AccountCode),
-                new SqlParameter("@AccountName", account.AccountName),
-                new SqlParameter("@AccountType", account.AccountType),
-                new SqlParameter("@Mode", account.Mode),
-                new SqlParameter("@IsUnderTrial", account.IsUnderTrial),
-                new SqlParameter("@TrialStartedOn", (object?)account.TrialStartedOn ?? DBNull.Value),
-                new SqlParameter("@TrialDuration", (object?)account.TrialDuration ?? DBNull.Value),
-                new SqlParameter("@TrialExpiredOn", (object?)account.TrialExpiredOn ?? DBNull.Value)
-            };
-
-            var result = await _context.Accounts
-                .FromSqlRaw("EXEC usp_Update_Account @AccountId, @AccountCode, @AccountName, @AccountType, @Mode, @IsUnderTrial, @TrialStartedOn, @TrialDuration, @TrialExpiredOn", 
-                    parameters)
-                .ToListAsync();
-
-            return result.FirstOrDefault() ?? account;
+            account.LastUpdated = DateTime.UtcNow;
+            _context.Accounts.Update(account);
+            await _context.SaveChangesAsync();
+            return account;
         }
 
         /// <summary>
-        /// Soft deletes an account using the usp_Delete_Account stored procedure
+        /// Soft deletes an account
         /// </summary>
         public async Task<int> DeleteAccountAsync(Guid accountId)
         {
-            var parameter = new SqlParameter("@AccountId", accountId);
-            
-            return await _context.Database
-                .ExecuteSqlRawAsync("EXEC usp_Delete_Account @AccountId", parameter);
+            var account = await _context.Accounts.FindAsync(accountId);
+            if (account == null)
+                return 0;
+
+            account.IsDeleted = true;
+            account.LastUpdated = DateTime.UtcNow;
+            await _context.SaveChangesAsync();
+            return 1;
         }
 
         /// <summary>
-        /// Retrieves an account by ID using the usp_Get_AccountById stored procedure
+        /// Retrieves an account by ID
         /// </summary>
         public async Task<Account?> GetAccountByIdAsync(Guid accountId)
         {
-            var parameter = new SqlParameter("@AccountId", accountId);
-            
-            var result = await _context.Accounts
-                .FromSqlRaw("EXEC usp_Get_AccountById @AccountId", parameter)
-                .ToListAsync();
-
-            return result.FirstOrDefault();
+            return await _context.Accounts
+                .Where(a => a.AccountId == accountId && !a.IsDeleted)
+                .FirstOrDefaultAsync();
         }
 
         /// <summary>
-        /// Retrieves all non-deleted accounts using the usp_Get_AllAccounts stored procedure
+        /// Retrieves all non-deleted accounts
         /// </summary>
         public async Task<IEnumerable<Account>> GetAllAccountsAsync()
         {
             return await _context.Accounts
-                .FromSqlRaw("EXEC usp_Get_AllAccounts")
+                .Where(a => !a.IsDeleted)
                 .ToListAsync();
         }
 
         /// <summary>
-        /// Retrieves an account by its unique code using the usp_Get_AccountByCode stored procedure
+        /// Retrieves an account by its unique code
         /// </summary>
         public async Task<Account?> GetAccountByCodeAsync(string accountCode)
         {
-            var parameter = new SqlParameter("@AccountCode", accountCode);
-            
-            var result = await _context.Accounts
-                .FromSqlRaw("EXEC usp_Get_AccountByCode @AccountCode", parameter)
-                .ToListAsync();
-
-            return result.FirstOrDefault();
+            return await _context.Accounts
+                .Where(a => a.AccountCode == accountCode && !a.IsDeleted)
+                .FirstOrDefaultAsync();
         }
     }
 }
